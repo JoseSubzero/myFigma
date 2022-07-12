a865a42bf0d450b02078766bddde3a0380738250
Alumno <dasdfasf>
2022-07-12 20:14:57 +0200

    Commit inicial. Añadido de textview, editview e imageview y edicion de las mismas mas el boton y segunda activity con el logo

---
+++ app/src/main/java/com/example/myfigma/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myfigma
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfigma.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
